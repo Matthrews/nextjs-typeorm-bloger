fd74218d81866fc0e86f94500a6307ddd860569e
Matthew <Matthrews@outlook.com>
2021-02-27 20:44:22 +0800

    '测试提交'

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,22 @@
-import Posts, {getServerSideProps} from './posts';
+import { NextPage } from "next";
+import Link from "next/link";
+import React from "react";
+import 'styles/Home.module.css'
 
-export default Posts;
+const Home: NextPage = () => {
+  return (
+    <div className="home-wrapper">
+      <div className="cover">
+        <img src="/logo.png" alt="Logo" />
+        <h1>我的个人博客网站</h1>
+        <p>
+          <Link href={`/posts`}>
+            <a>文章列表</a>
+          </Link>
+        </p>
+      </div>
+    </div>
+  );
+};
 
-export {getServerSideProps};
+export default Home;
